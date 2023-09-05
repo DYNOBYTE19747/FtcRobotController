3618af4bfbac39c09b995546a070b3c6bd933aa3
harshN26 <143971899+DYNOBYTE19747@users.noreply.github.com>
2023-09-05 11:35:40 -0400

    this is a test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class Test extends LinearOpMode {
+    DcMotor Left;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        ;
+    }
+}
