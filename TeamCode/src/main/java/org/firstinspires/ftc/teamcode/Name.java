ebe32106818335d7d9572bd0b9cef5b6d8906f40
Arav_Patel <aravpatel3017362@gmail.com>
2023-09-05 11:28:10 -0400

    Test_Push
    Test push to repo-Arav

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Name.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class Name extends LinearOpMode{
+    DcMotor d1;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        ;
+    }
+}
